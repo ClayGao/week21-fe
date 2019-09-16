237e0a6f3545e58566b1b2c17f57dc65dec4540f
ClayGao <r700000@gmail.com>
2019-09-17 02:27:31 +0800

    hw3 update

--- blog/src/Blog.js
+++ blog/src/Blog.js
@@ -1,88 +1,122 @@
 import React, {Component} from 'react'
 import './style.scss'
-import getData from './ajax'
 
-class List extends Component {
-    render() {
-        return (
-            <span>List</span>
-        )
-    }
-}
 
-class InfoPage extends Component {
-    render(){
-        return (
-            <span>About</span>
-        )
-    }
+function InfoPage(props) {
+    return (
+        <div  className="board">
+            <div className="article">
+                <div className="article-title">
+                    Hi, Here is Blue Orange
+                </div>
+                <div className="article-editor">
+                    第二十一週作業作業做很久 冏
+                    第二十一週作業作業做很久 冏第二十一週作業作業做很久 冏
+                    第二十一週作業作業做很久 冏
+                    第二十一週作業作業做很久 冏
+                    第二十一週作業作業做很久 冏
+                    第二十一週作業作業做很久 冏
+                    第二十一週作業作業做很久 冏
+                    第二十一週作業作業做很久 冏
+                </div>
+            </div>
+        </div>
+    )
 }
 
 class Article extends Component {
     constructor(props) {
         super(props)
+        this.state = {
+            isSingleArticle: false,
+            articleData: []
+        }
+        this.handleServerData = this.handleServerData.bind(this)
+    }
+
+    handleServerData() {
         let url = 'https://jsonplaceholder.typicode.com/posts'
-        let articleData = []
-
-        
-
-        fetch(url, {method: 'get'})
-            .then(function(resp) {
-                console.log(resp.json())
+        fetch(url)
+            .then(resp => {
                 return resp.json()
             })
-            .then(function(jsonData){
-                infiState(jsonData);
+            .then(jsonData => {
+                 this.setState({
+                        isSingleArticle: false,
+                        articleData: jsonData
+                 })
             })
-        
-            
+    }
+    
+    handleSingleServerData(id = '') {
+        let url = 'https://jsonplaceholder.typicode.com/posts'
+        fetch(url + id)
+            .then(resp => {
+                return resp.json()
+            })
+            .then(jsonData => {
+                 this.setState({
+                        isSingleArticle: true,
+                        articleData: jsonData
+                 })
+            })
+    }
+    
 
-        
+    componentDidMount() {
+        this.handleServerData()
     }
     
     render(){
-        console.log(this.state)
+        const {isSingleArticle, articleData} = this.state
         return (
-            <div className="article">
-                <div className="article-title">
-                    4564564564564564564564564
+            <div  className="board">
+            {articleData.map(card => (
+                <div className="article" onClick={this.handleSingleServerData.bind(this,'?id='+ card.id)}>
+                    <div className="article-title">
+                        {card.title}
+                    </div>
+                    {isSingleArticle && <div className="article-text">{card.body}</div>}
+                    <div className="article-editor">
+                        Editor: {card.userId}
+                    </div>
+                    {isSingleArticle && <div className="article-back" onClick={this.handleServerData}>Go back</div>}
                 </div>
-                <div className="article-text">
-                    4564565646456
-                    55645646456
-                    456456
-                    4564565646456
-                    55645646456
-                    456456
-                    4564565646456
-                    55645646456
-                    456456
-                </div>
-                <div className="article-editor">
-                    Clay
-                </div>
+            ))}
             </div>
         )
     }
 }
 
 class Blog extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            displayMode : 'list',
+        }
+    }
+
+    handleDisplayMode(mode) {
+        this.setState({ displayMode : mode })
+    }
+
     render() {
+        console.log(this.state.displayMode)
+        const displayMode = this.state.displayMode
         return (
             <div className="Blog">
-            <nav>
-                <span>ClayGao's Blog</span>
-                <List />
-                <InfoPage />
-            </nav>
-            <div className="wrapper">
-                <div className="board">
-                    <Article />
+                <nav>
+                    <span>ClayGao's Blog</span>
+                    <span onClick={this.handleDisplayMode.bind(this, 'info')}>About</span>
+                    <span onClick={this.handleDisplayMode.bind(this, 'list')}>List</span>
+                </nav>
+                <div className="wrapper">
+                        {displayMode === 'list' && <Article handeDisplayMode={this.handleDisplayMode} />}
+                        {displayMode === 'info' && <InfoPage />}
                 </div>
-            </div>
-            <footer>
-                Bg-image By Unsplash
-            </footer>
+                <footer>
+                    Bg-image By Unsplash
+                </footer>
             </div>
         )
     }
